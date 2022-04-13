7e8e7af19f542acb9f7ce5e18c50ecbe771327c0
mittamahesh <100993658+mittamahesh@users.noreply.github.com>
2022-04-13 21:40:00 +0530

    Add files via upload

---
+++ cppday1/cppnumbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=50;
+    cout <<40 <<endl;
+    cout << 5+7 <<endl;
+    cout <<5-7 <<endl;
+    cout <<5*7 <<endl;
+    cout <<5/10 <<endl;
+    cout <<5%2 <<endl;
+    n++;
+    cout <<n;
+    return 0;
+}
