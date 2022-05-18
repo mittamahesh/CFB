59cb4eae508f3827fce16edca2e9bc18255eb54e
mittamahesh <100993658+mittamahesh@users.noreply.github.com>
2022-05-18 23:05:15 +0530

    Add files via upload

---
+++ streak3/react day1/Greet.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Greet() {
+    return <h1>hello mahesh<p>using functional components</p></h1>
+}
+
+export default Greet
---
+++ streak3/react day1/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Welcome extends Component
+{
+    render(){
+        return <h1>Class components</h1>
+        return<p>using Class components</p>
+    }
+}
+
+export default Welcome
